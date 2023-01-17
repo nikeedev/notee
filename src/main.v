a446cbe777b599824681a6276f4bcc5a308efa62
nikeedev <myftr12394@gmail.com>
2023-01-17 16:24:14 +0100

    updates

--- src/main.v
+++ src/main.v
@@ -2,6 +2,14 @@ module main
 
 import term
 import term.ui as tui
+import os
+
+struct AppConfig {
+mut:
+	editor_mode bool = false
+}
+
+const appcfg = AppConfig{}
 
 struct App {
 mut:
@@ -9,19 +17,26 @@ mut:
 }
 
 fn event(e &tui.Event, x voidptr) {
-    if e.typ == .key_down && e.code == .escape {
+    if e.typ == .key_down && e.code == .q && !appcfg.editor_mode {
         exit(0)
+    }
+	if e.typ == .key_down && e.code == .i && !appcfg.editor_mode  {
+        appcfg.editor_mode = true
+    }
+    if e.typ == .key_down && e.code == .escape && !appcfg.editor_mode  {
+        appcfg.editor_mode = true
     }
 }
 
 fn frame(x voidptr) {
     mut app := &App(x)
-
+	mut text :=
     app.tui.clear()
-    app.tui.set_bg_color(r: 63, g: 81, b: 181)
-    app.tui.draw_rect(20, 6, 41, 10)
-    app.tui.draw_text(24, 8, 'Hello from V!')
-    app.tui.set_cursor_position(0, 0)
+    app.tui.draw_text(0, 0, '---- notee ----')
+
+	if appcfg.editor_mode {
+		term.input()
+	    app.tui.set_cursor_position(0, 0)
 
     app.tui.reset()
     app.tui.flush()
@@ -34,6 +49,9 @@ fn main() {
         event_fn: event
         frame_fn: frame
         hide_cursor: true
-    )
+		window_title: term.header("notee", " ")
+	)
+
     app.tui.run()?
+
 }
